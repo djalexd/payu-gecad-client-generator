d6470323e710aecbeb4e9bbaebc4af3e5ab57a75
alexdobjanschi <alex.dobjanschi@gmail.com>
2016-11-06 17:16:11 +0100

    Updated java codegen version

--- src/main/java/com/payu/gecad/alu/swagger/plugin/PayUGecadJavaClientCodegen.java
+++ src/main/java/com/payu/gecad/alu/swagger/plugin/PayUGecadJavaClientCodegen.java
@@ -12,17 +12,19 @@ public class PayUGecadJavaClientCodegen extends JavaClientCodegen {
 	@Override
 	public void processOpts() {
 		super.processOpts();
+
+		additionalProperties.put("gson", "false");
+		additionalProperties.put("jackson", "true");
+
+		supportingFiles.clear();
 		final String invokerFolder = (sourceFolder + '/' + invokerPackage).replace(".", "/");
-		supportingFiles.add(new SupportingFile("Hashing.mustache", invokerFolder, "Hashing.java"));
-		supportingFiles.add(new SupportingFile("XML.mustache", invokerFolder, "XML.java"));
-		supportingFiles.add(new SupportingFile("TypeConverter.mustache", invokerFolder, "TypeConverter.java"));
-		supportingFiles.add(new SupportingFile("TypeConverterRegistry.mustache", invokerFolder, "TypeConverterRegistry.java"));
-		supportingFiles.add(new SupportingFile("Convertors.mustache", invokerFolder, "Convertors.java"));
-
-		final String apiFolder = (sourceFolder + '/' + apiPackage).replace(".", "/");
-		supportingFiles.add(new SupportingFile("alu.mustache", apiFolder, "Alu.java"));
-
 		final String modelFolder = (sourceFolder + '/' + modelPackage).replace(".", "/");
+
+		writeOptional(outputFolder, new SupportingFile("pom.mustache", "", "pom.xml"));
+		writeOptional(outputFolder, new SupportingFile("README.mustache", "", "README.md"));
+		supportingFiles.add(new SupportingFile("ApiResponse.mustache", invokerFolder, "ApiResponse.java"));
+		supportingFiles.add(new SupportingFile("ApiException.mustache", invokerFolder, "ApiException.java"));
+		supportingFiles.add(new SupportingFile("Hashing.mustache", invokerFolder, "Hashing.java"));
 		supportingFiles.add(new SupportingFile("UTCDateTimeAdapter.mustache", modelFolder, "UTCDateTimeAdapter.java"));
 
 		importMapping.put("Stream", "java.util.stream.Stream");
